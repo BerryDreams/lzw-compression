bccdd6c67b5c0e6c20b9bbf62e969fc3a934446c
misxress <misxress@outlook.com>
2020-03-28 09:41:22 +0800

    第一次commit

---
+++ cmake-build-debug/CMakeFiles/compression.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/compression.dir/bmp.cpp.obj"
+  "CMakeFiles/compression.dir/lzw.cpp.obj"
+  "CMakeFiles/compression.dir/test.cpp.obj"
+  "CMakeFiles/compression.dir/txt.cpp.obj"
+  "compression.exe"
+  "compression.exe.manifest"
+  "compression.pdb"
+  "libcompression.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/compression.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
